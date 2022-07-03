88529453e31d6fc26e89097bf88c7315dd571e88
s-higuchi <47686127+hsin5@users.noreply.github.com>
2022-07-03 21:15:57 +0900

    chore: SwiftPM

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let package = Package(
             name: "AlamofireSwiftyJSON",
             targets: ["AlamofireSwiftyJSON"])
   ],
-  dependencies: [
-      .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4),
-      .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", majorVersion: 5)
-  ],
+  //dependencies: [
+  //    .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4),
+  //    .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", majorVersion: 5)
+  //],
   targets: [
       .target(name: "AlamofireSwiftyJSON")
   ]
