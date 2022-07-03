22e3671c3e1622fb9bc2e5fdcd910b132c99e919
s-higuchi <47686127+hsin5@users.noreply.github.com>
2022-07-03 21:07:13 +0900

    chore: SwiftPM

--- Package.swift
+++ Package.swift
@@ -4,6 +4,16 @@ import PackageDescription
 
 let package = Package(
   name: "AlamofireSwiftyJSON",
+  platforms: [.macOS(.v10_10),
+              .iOS(.v9),
+              .tvOS(.v9),
+              .watchOS(.v2)],
+  products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AlamofireSwiftyJSON",
+            targets: ["AlamofireSwiftyJSON"]),
+  ],
   dependencies: [
       .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4),
       .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", majorVersion: 5)
